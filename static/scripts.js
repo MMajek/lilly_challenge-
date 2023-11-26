87d6b8ddb33c899277d1d185d652d36149d9c847
Michael <majekmichael3@gmail.com>
2023-11-26 14:59:11 +0000

    small change

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,7 @@
+const stocks = require("../stocks.js");
 const canvas = document.getElementById('chart')
 const ctx = canvas.getContext('2d')
+
 
 function drawLine (start, end, style) {
   ctx.beginPath()
@@ -22,3 +24,4 @@ drawTriangle([35, 50], [65, 50], [50, 35])
 
 drawLine([50, 550], [950, 550])
 drawTriangle([950, 535], [950, 565], [965, 550])
+console.log(stocks);
